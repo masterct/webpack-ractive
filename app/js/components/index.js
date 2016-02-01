be5ca37ad95f241309b77f0dee5347465722242a
Mohamed-Cherif Turki <mohamed-cherif.turki-ext@amundi.com>
2016-02-01 17:43:44 +0100

    Added ractiveRouter Component

--- app/js/components/index.js
+++ app/js/components/index.js
@@ -4,11 +4,13 @@ import form from './form';
 import navBar from './navBar';
 import imgGallery from './imgGallery';
 import blueimpGallery from './blueimpGallery';
+import ractiveRouter from './ractiveRouter';
 
 export default {
   carousel: carousel,
   myFooter: footer,
   formComp: form,
   navBar: navBar,
-  blueimpGallery: blueimpGallery
+  blueimpGallery: blueimpGallery,
+  ractiveRouter: ractiveRouter
 };
---
+++ app/js/components/ractiveRouter/index.js
@@ -0,0 +1,17 @@
+import Ractive from 'ractive';
+
+var ractiveRouter = Ractive.extend({
+    template: '<component/>',
+    components: {
+      component: function(){
+        return this.get('name');
+      }
+    },
+    oninit: function () {
+      this.observe('name', function () {
+        this.reset();
+      }, {init: false});
+    }
+});
+
+export default ractiveRouter;
