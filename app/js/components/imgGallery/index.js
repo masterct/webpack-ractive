9896b9d6e71447f1f85630f950ca47b4183211aa
Mohamed-Cherif Turki <mohamed-cherif.turki-ext@amundi.com>
2016-02-01 16:22:40 +0100

    testing out different galleries

---
+++ app/js/components/imgGallery/index.js
@@ -0,0 +1,8 @@
+import Ractive from 'ractive';
+import Template from '../../../views/components/imgGallery.html';
+
+var imgGallery = Ractive.extend({
+    template: Template
+});
+
+export default imgGallery;
--- app/js/components/index.js
+++ app/js/components/index.js
@@ -2,10 +2,12 @@ import carousel from './carousel';
 import footer from './footer';
 import form from './form';
 import navBar from './navBar';
+import imgGallery from './imgGallery';
 
 export default {
   carousel: carousel,
   myFooter: footer,
   formComp: form,
-  navBar: navBar
+  navBar: navBar,
+  imgGallery: imgGallery
 };
