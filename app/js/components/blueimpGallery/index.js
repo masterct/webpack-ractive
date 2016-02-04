2435646e41acdc9feeb8286f7565ae242835d12d
masterct <mohamed.cherif.turki@gmail.com>
2016-02-04 16:20:32 +0100

    added home, safe and unsafe images

--- app/js/components/blueimpGallery/index.js
+++ app/js/components/blueimpGallery/index.js
@@ -2,7 +2,87 @@ import Ractive from 'ractive';
 import Template from '../../../views/components/blueimp.html';
 
 var blueimpGallery = Ractive.extend({
-    template: Template
+    template: Template,
+    data: function() {
+      return {
+        images: [{
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }, {
+          name: 'Title',
+          src: 'img/safe',
+          width: '100',
+          height: '100'
+        }]
+      };
+    }
 });
 
 export default blueimpGallery;
